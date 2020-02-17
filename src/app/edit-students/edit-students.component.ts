3fcce65108a9dc1d90ba5f800378b07cdccae2db
rohanmorris <sirrom2005@gmail.com>
2020-02-17 00:27:48 -0500

    ..

--- src/app/edit-students/edit-students.component.ts
+++ src/app/edit-students/edit-students.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { IStudent } from "../Interface/IStudent";
 import { AppGlobals } from "../common/app-globals";
@@ -15,13 +15,16 @@ import { IKeyValue } from '../Interface/IValueKey';
 
 export class EditStudentsComponent implements OnInit {
   studentid:String;
-  Students:IStudent;
+  Students = {};
   formData:FormGroup;
   extraCurricular:IKeyValue;
   responsibilities:IKeyValue;
+  classList:IKeyValue;
+  houseColor:IKeyValue;
+  defaultSchooolId = 2011040016;
 
   constructor(private formBuilder:FormBuilder, private http:HttpClient, private route: ActivatedRoute) { 
-    this.formData = this.formBuilder.group({
+    /*this.formData = this.formBuilder.group({
       acconut_id: ['', Validators.required],
       firstname: ['', Validators.required],
       middlename: ['', Validators.required],
@@ -29,39 +32,51 @@ export class EditStudentsComponent implements OnInit {
       gender: ['', Validators.required],
       dob: ['', Validators.required],
       enrollment_date: ['', Validators.required]
-    });
+    });*/
   }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => { 
-      this.studentid = params.get('student_id'); 
-      this.http.get<IStudent>(AppGlobals.API_DOMAIN + '/students/' + this.studentid).
-      subscribe(data => {
-        this.Students = data;
-      });
+    //House Color List
+    this.http.get<IKeyValue>(AppGlobals.API_DOMAIN + '/valuekey/housecolor/' + this.defaultSchooolId)
+    .subscribe(data => {
+      this.houseColor = data;
     });
 
     //Extra Curricular Activity List
     this.http.get<IKeyValue>(AppGlobals.API_DOMAIN + '/valuekey/extracurricularactivity')
       .subscribe(data => {
         this.extraCurricular = data;
-      });
+    });
+
+    //Class List by school id
+    this.http.get<IKeyValue>(AppGlobals.API_DOMAIN + '/valuekey/class/' + this.defaultSchooolId)
+    .subscribe(data => {
+      this.classList = data;
+    });
 
     //Responsibilities List
     this.http.get<IKeyValue>(AppGlobals.API_DOMAIN + '/valuekey/responsibilities')
     .subscribe(data => {
       this.responsibilities = data;
     });
+
+    //Load Account data
+    this.route.paramMap.subscribe(params => { 
+      this.studentid = params.get('student_id'); 
+      this.http.get(AppGlobals.API_DOMAIN + '/accounts/' + this.studentid).
+      subscribe(data => {
+        this.Students = data;
+      });
+    });
   }
 
-  onSubmit(body) {
-    const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
-    const url = AppGlobals.API_DOMAIN + '/students/';
+  onSubmit(account) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const url = AppGlobals.API_DOMAIN + '/accounts/' + account.acconut_id;
        
-    this.http.post<IStudent>(url, body, {headers, responseType:"json"})
+    this.http.put(url, account, {headers, responseType:"json"})
     .subscribe(data => {
       console.log(data);
     });
   }
-
 }
